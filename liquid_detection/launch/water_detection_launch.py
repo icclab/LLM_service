c6a1b1fd72bfe6bdf06cec680dbaaf6881545969
Sisqui <aries94leifu@gmail.com>
2025-04-10 16:45:41 +0200

    update liquid position

--- liquid_detection/launch/water_detection_launch.py
+++ liquid_detection/launch/water_detection_launch.py
@@ -8,9 +8,15 @@ def generate_launch_description():
             executable='roboflow',  
             name='roboflow_water',
             output='screen',
+            remappings=[
+                ('/tf', '/summit/tf'),
+                ('/tf_static', '/summit/tf_static'),
+                ('/leakage_marker', '/summit/leakage_marker'),
+                ('/leakage_annotated_image', '/summit/leakage_annotated_image'),
+            ],
             parameters=[
-                {'image_topic': '/summit/oak/rgb/image_rect'},
-                {'gps_topic': '/summit/gps/fix'},
-                {'local_topic': '/summit/base_pose'},]
+                {'image_topic': '/summit/color/image'},
+                {'depth_topic': '/summit/stereo/depth'},
+                {'camera_info_topic': '/summit/color/camera_info'},]
         ),
     ])
--- liquid_detection/liquid_detection/roboflow_water.py
+++ liquid_detection/liquid_detection/roboflow_water.py
@@ -2,7 +2,7 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image as ROSImage
 from sensor_msgs.msg import NavSatFix, CameraInfo
-from geometry_msgs.msg import Point, PoseStamped
+from geometry_msgs.msg import Point, PoseStamped, PointStamped
 from nav_msgs.msg import Odometry
 from visualization_msgs.msg import Marker
 from cv_bridge import CvBridge
@@ -23,15 +23,11 @@ class LeakageDetection(Node):
         super().__init__('leakage_detection_client')
         
         # Declare parameters with default values
-        self.declare_parameter('image_topic', '/summit/front_rgbd_camera/color/image_raw')
-        self.declare_parameter('gps_topic', '/summit/gps/fix')
-        self.declare_parameter('local_topic', '/summit/base_pose')
+        self.declare_parameter('image_topic', '/summit/color/image')
         self.declare_parameter('depth_topic', '/summit/stereo/depth')
         self.declare_parameter('camera_info_topic', '/summit/color/camera_info')
 
         image_topic = self.get_parameter('image_topic').get_parameter_value().string_value
-        gps_topic = self.get_parameter('gps_topic').get_parameter_value().string_value
-        local_topic = self.get_parameter('local_topic').get_parameter_value().string_value
         depth_topic = self.get_parameter('depth_topic').get_parameter_value().string_value
         camera_info_topic = self.get_parameter('camera_info_topic').get_parameter_value().string_value
 
@@ -47,21 +43,14 @@ class LeakageDetection(Node):
         self.camera_info_sub = self.create_subscription(CameraInfo, camera_info_topic, self.camera_info_callback, 10)
 
         self.image_subscription = Subscriber(self, ROSImage, image_topic, qos_profile=qos_profile)
-        # self.gps_subscription = Subscriber(self, NavSatFix, gps_topic, qos_profile=qos_profile)
-        self.local_subscription = Subscriber(self, PoseStamped, local_topic, qos_profile=qos_profile)
         self.depth_subscription = Subscriber(self, ROSImage, depth_topic, qos_profile=qos_profile)
 
-        # self.sync = ApproximateTimeSynchronizer([self.image_subscription, self.gps_subscription, self.local_subscription], queue_size=10, slop=0.1)
-        self.sync = ApproximateTimeSynchronizer([self.image_subscription, self.depth_subscription, self.local_subscription], queue_size=10, slop=0.1)
+        self.sync = ApproximateTimeSynchronizer([self.image_subscription, self.depth_subscription], queue_size=10, slop=0.1)
         self.sync.registerCallback(self.synchronized_callback)
         
-        self.leakage_gps_pub = self.create_publisher(Point, '/leakage_gps', 10)
-        self.leakage_local_pub = self.create_publisher(Point, '/leakage_local', 10)
         self.marker_pub = self.create_publisher(Marker, '/leakage_marker', 10)
         self.annotated_image_pub = self.create_publisher(ROSImage, '/leakage_annotated_image', 10)
         
-        # self.latest_gps = None
-        self.latest_local = None
         self.depth_img = None
         self.model_client = InferenceHTTPClient(
             api_url="https://roboflow.nephele-project.ch",
@@ -70,22 +59,22 @@ class LeakageDetection(Node):
 
         self.models = [
             #("water-leakage/2", sv.Color(255, 0, 0)),  # Red
+            # ("persons-muimi/8", sv.Color(255, 0, 0)),  # Red
             ("water-ba8zz/1", sv.Color(0, 255, 0)),  # Green
-            ("spills-ax5xv/2", sv.Color(0, 0, 255))  # Blue
+            # ("spills-ax5xv/2", sv.Color(0, 0, 255))  # Blue
         ]
 
     def camera_info_callback(self, camera_info_msg):
         """ Initialize camera model from CameraInfo. """
         self.camera_model.fromCameraInfo(camera_info_msg)
 
-    # def synchronized_callback(self, ros_image: ROSImage, gps_msg: NavSatFix, local_pos_msg: PoseStamped):
-    def synchronized_callback(self, ros_image: ROSImage, depth_image: ROSImage, local_pos_msg: PoseStamped):
-        self.get_logger().info('Image and Pose received, running inference...')
+
+    def synchronized_callback(self, ros_image: ROSImage, depth_image: ROSImage):
+    # def synchronized_callback(self, ros_image: ROSImage, local_pos_msg: PoseStamped):
+        self.get_logger().info('Image and depth received, running inference...')
 
         cv_image = self.bridge.imgmsg_to_cv2(ros_image, desired_encoding='bgr8')
-        self.latest_local = local_pos_msg
         self.depth_img = self.bridge.imgmsg_to_cv2(depth_image)
-        # self.latest_gps = gps_msg
 
         detections = []
 
@@ -100,46 +89,47 @@ class LeakageDetection(Node):
 
         self.get_logger().info('Leakage detected! Publishing last known position...')
 
-        for det in actual_detections[0]:
-            center_x = int(det.x)
-            center_y = int(det.y)
+        for detection in actual_detections:
+            for bbx in detection[0][0].xyxy:
+                x1, y1, x2, y2 = bbx
+                center_x = int((x1 + x2) / 2)
+                center_y = int((y1 + y2) / 2)
 
-            if self.depth_img is not None:
-                try:
-                    depth_value = self.depth_img[center_y, center_x]
-                    if depth_value == 0:
-                        self.get_logger().warn(f"Invalid depth at pixel ({center_x}, {center_y})")
+                if self.depth_img is not None:
+                    try:
+                        depth_value = self.depth_img[center_y, center_x] * 0.001
+                        if depth_value <= 0 or np.isnan(depth_value):
+                            self.get_logger().warn(f"Invalid depth at pixel ({center_x}, {center_y})")
+                            continue
+
+                        ray = self.camera_model.projectPixelTo3dRay((center_x, center_y))
+                        ray = np.array(ray)
+                        point_3d = ray * (depth_value / ray[2])
+
+                        self.get_logger().info(f"Detection center at pixel ({center_x}, {center_y}) => 3D point: {point_3d}")
+
+                        # Transform the 3D point to the map frame
+                        target_frame = 'map'
+                        source_frame = 'oak_rgb_camera_optical_frame'
+
+                        leakage_msg = PointStamped()
+                        leakage_msg.header.frame_id = source_frame
+                        leakage_msg.header.stamp = self.get_clock().now().to_msg()
+                        leakage_msg.point.x = point_3d[0]
+                        leakage_msg.point.y = point_3d[1]
+                        leakage_msg.point.z = point_3d[2]
+                        
+                        transform = self.tf_buffer.lookup_transform(target_frame, source_frame, rclpy.time.Time().to_msg(), timeout=rclpy.duration.Duration(seconds=5.0))
+                        transformed_point = do_transform_point(leakage_msg, transform)
+
+                        self.publish_leakage_marker(transformed_point.point)
+
+                    except Exception as e:
+                        self.get_logger().error(f"Error processing 3D point: {e}")
                         continue
+                else:
+                    self.get_logger().warn("No depth image received yet.")
 
-                    ray = self.camera_model.projectPixelTo3dRay((center_x, center_y))
-                    ray = np.array(ray)
-                    point_3d = ray * (depth_value / ray[2])
-
-                    self.get_logger().info(f"Detection center at pixel ({center_x}, {center_y}) => 3D point: {point_3d}")
-                    
-                    leakage_msg = Point(x=point_3d[0], y=point_3d[1], z=point_3d[2])
-                    target_frame = "map"
-                    source_frame = "oak_rgb_camera_optical_frame"
-                    transform = self.tf_buffer.lookup_transform(target_frame, source_frame, rclpy.time.Time().to_msg(), timeout=rclpy.duration.Duration(seconds=5.0))
-                    transformed_point = do_transform_point(leakage_msg, transform)
-
-                    self.publish_leakage_marker(transformed_point.point)
-
-                except Exception as e:
-                    self.get_logger().error(f"Error processing 3D point: {e}")
-                    continue
-            else:
-                self.get_logger().warn("No depth image received yet.")
-           
-        # if self.latest_local:
-        #     leakage_local_msg = Point(x=self.latest_local.pose.position.x, y=self.latest_local.pose.position.y, z=self.latest_local.pose.position.z)
-        #     self.leakage_local_pub.publish(leakage_local_msg)
-
-        # if self.latest_gps:
-        #     leakage_gps_msg = Point(x=self.latest_gps.latitude, y=self.latest_gps.longitude, z=self.latest_gps.altitude)
-        #     self.leakage_gps_pub.publish(leakage_gps_msg)
-
-        # self.publish_leakage_marker(self.latest_local.pose.position)
 
         # Annotate image with bounding boxes and labels
         annotated_image = self.annotate_image(cv_image, actual_detections)
@@ -153,8 +143,12 @@ class LeakageDetection(Node):
             box_annotator = sv.BoxAnnotator(color=color)
             label_annotator = sv.LabelAnnotator(color=color)
 
+            labels = [
+                f"{class_name} {confidence:.2f}"
+                for class_name, confidence in zip(detection['class_name'], detection.confidence)]
+
             annotated_image = box_annotator.annotate(scene=annotated_image, detections=detection)
-            annotated_image = label_annotator.annotate(scene=annotated_image, detections=detection)
+            annotated_image = label_annotator.annotate(scene=annotated_image, detections=detection, labels=labels)
 
         sv.plot_image(annotated_image)
         return annotated_image
